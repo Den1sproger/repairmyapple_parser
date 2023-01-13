f84c97afc8e3a29efac4d893ec8db7dc0abf371d
Den1sproger <114018798+Den1sproger@users.noreply.github.com>
2023-01-13 23:58:23 +0300

    refactor: apply object oriented paradigm

--- telegram_bot.py
+++ telegram_bot.py
@@ -5,23 +5,112 @@ from telebot.types import Message, ReplyKeyboardMarkup
 
 
 
-TOKEN = '5320242884:AAFvo5kvfeHoHZZNa6xbvhvuHMGZmjbkdF8'
-
-
+TOKEN = 'TOKEN'
 
 bot = telebot.TeleBot(TOKEN)
 
 
-model = str
-max_price = int
-diagonal = int
-memory = int
+
+class Getting_Device:
+    """"""
+
+    def __init__(self, device: object,
+                 apple_watch: bool = False) -> None:
+        self.device = device
+        self.apple_watch = apple_watch
+        self.model = str
+        self.max_price = int
+        if apple_watch:
+            self.diagonal = int
+        else:
+            self.memory = int
 
 
+    @staticmethod
+    def get_card(product: dict) -> str:
+        return f'{product.get("text")}\n{product.get("link")}\n'\
+            f'Price: {product.get("price")} rub'
 
-def get_card(product: dict) -> str:
-    return f'{product.get("text")}\n{product.get("link")}\n'\
-           f'Price: {product.get("price")} rub'
+
+    def choose_model(self, message: Message) -> None:
+        kb = ReplyKeyboardMarkup(resize_keyboard=True)
+        kb.add(*self.device.models)
+        bot.send_message(
+            message.chat.id, 'Please, choose model', reply_markup=kb
+        )
+        bot.register_next_step_handler(message, self.input_max_price)
+
+
+    def input_max_price(self, message: Message) -> None:
+        self.model = message.text
+        bot.send_message(
+            message.chat.id, 'Enter the maximum allowable price (in rubles)'
+        )
+        if self.apple_watch:
+            bot.register_next_step_handler(message, self.input_diag)
+        else:
+            bot.register_next_step_handler(message, self.choose_memory)
+            
+
+    def input_diag(self, message: Message) -> None:
+        try:
+            self.max_price = int(message.text)
+        except ValueError:
+            bot.reply_to(message, 'Incorrected input!')
+        else:
+            bot.send_message(
+                message.chat.id, 'Enter the screen diagonal (in millimeters)'
+            )
+            bot.register_next_step_handler(message, self.get_devices)
+
+
+    def choose_memory(self, message: Message) -> None:
+        try:
+            self.max_price = int(message.text)
+        except ValueError:
+            bot.reply_to(message, 'Incorrected input!')
+        else:
+            memory_buttons = [str(2 ** i) + ' Gb' for i in range(1, 12)]
+            kb = ReplyKeyboardMarkup(resize_keyboard=True)
+            kb.add(*memory_buttons)
+            bot.send_message(
+                message.chat.id, 'Choose the device memory', reply_markup=kb
+            )
+            bot.register_next_step_handler(message, self.get_devices)
+
+
+    def get_devices(self, message: Message) -> None:
+        try:
+            if self.apple_watch:
+                self.diagonal = int(message.text)
+            else:
+                self.memory = int(message.text.replace(' Gb', ''))
+        except ValueError:
+            bot.reply_to(message, 'Incorrected input!')
+        else:
+            bot.send_message(message.chat.id, 'Please, waiting...')
+
+            if self.apple_watch:
+                devices = self.device.get_products(
+                    url=str(self.device.models.get(self.model)),
+                    max_price=int(self.max_price),
+                    desired_diagonal=self.diagonal
+                )
+            else:
+                devices = self.device.get_products(
+                    url=str(self.device.models.get(self.model)),
+                    max_price=int(self.max_price),
+                    desired_memory=self.memory
+                )
+                
+            for i in devices:
+                card = Getting_Device.get_card(i)
+                bot.send_message(message.chat.id, card)
+
+    
+    def __del__(self) -> int:
+        return 0
+
 
 
 @bot.message_handler(commands=['start', 'search'])
@@ -34,123 +123,18 @@ def start(message: Message) -> None:
     )
 
 
-
 @bot.message_handler(func=lambda message: message.text == 'IPhone')
-def choose_iph_model(message: Message) -> None:
-    global search
-    search = IPhone()
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(*search.models)
-    bot.send_message(
-        message.chat.id, 'Please, choose model', reply_markup=kb
-    )
-    bot.register_next_step_handler(message, input_mp_iph)
-
-
-def input_mp_iph(message: Message) -> None:
-    global model
-    model = message.text
-    bot.send_message(
-        message.chat.id, 'Enter the maximum allowable price (in rubles)'
-    )
-    bot.register_next_step_handler(message, choose_iph_memory)
-
-
-def choose_iph_memory(message: Message) -> None:
-    global max_price
-    try:
-        max_price = int(message.text)
-    except ValueError:
-        bot.reply_to(message, 'Incorrected input!')
-    else:
-        memory_buttons = [str(2 ** i) + ' Gb' for i in range(1, 12)]
-        kb = ReplyKeyboardMarkup(resize_keyboard=True)
-        kb.add(*memory_buttons)
-        bot.send_message(
-            message.chat.id, 'Choose the device memory', reply_markup=kb
-        )
-        bot.register_next_step_handler(message, get_iphones)
-
-
-def get_iphones(message: Message) -> None:
-    global model, max_price, memory
-    try:
-        memory = int(message.text.replace(' Gb', ''))
-    except ValueError:
-        bot.reply_to(message, 'Incorrected input!')
-    else:
-        bot.send_message(message.chat.id, 'Please, waiting...')
-
-        search = IPhone()
-        iphones = search.get_products(
-            url=str(search.models.get(model)),
-            max_price=int(max_price),
-            desired_memory=memory
-        )
-
-        for i in iphones:
-            card = get_card(i)
-            bot.send_message(message.chat.id, card)
-    finally:
-        model, max_price, memory = '', 0, 0
-
+def get_iphones(message):
+    iphone = IPhone()
+    search = Getting_Device(iphone)
+    search.choose_model(message)
 
 
 @bot.message_handler(func=lambda message: message.text == 'MacBook')
-def choose_mb_model(message: Message) -> None:
-    search = MacBook()
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(*search.models)
-    bot.send_message(
-        message.chat.id, 'Please, choose model', reply_markup=kb
-    )
-    bot.register_next_step_handler(message, input_mp_mcbk)
-
-def input_mp_mcbk(message: Message) -> None:
-    global model
-    model = message.text
-    bot.send_message(
-        message.chat.id, 'Enter the maximum allowable price (in rubles)'
-    )
-    bot.register_next_step_handler(message, choose_mb_memory)
-
-
-def choose_mb_memory(message: Message) -> None:
-    global max_price
-    try:
-        max_price = int(message.text)
-    except ValueError:
-        bot.reply_to(message, 'Incorrected input!')
-    else:
-        memory_buttons = [str(2 ** i) + ' Gb' for i in range(1, 12)]
-        kb = ReplyKeyboardMarkup(resize_keyboard=True)
-        kb.add(*memory_buttons)
-        bot.send_message(
-            message.chat.id, 'Choose the device memory', reply_markup=kb
-        )
-        bot.register_next_step_handler(message, get_macbooks)
-
-
-def get_macbooks(message: Message) -> None:
-    global model, max_price, memory
-    try:
-        memory = int(message.text.replace(' Gb', ''))
-    except ValueError:
-        bot.reply_to(message, 'Incorrected input!')
-    else:
-        bot.send_message(message.chat.id, 'Please, waiting...')
-        search = MacBook()
-        macbooks = search.get_products(
-            url=str(search.models.get(model)),
-            max_price=int(max_price),
-            desired_memory=memory
-        )
-
-        for i in macbooks:
-            card = get_card(i)
-            bot.send_message(message.chat.id, card)
-    finally:
-        model, max_price, memory = '', 0, 0
+def get_macbooks(message):
+    macbook = MacBook()
+    search = Getting_Device(macbook)
+    search.choose_model(message)
 
 
 @bot.message_handler(func=lambda message: message.text == 'AirPods')
@@ -161,63 +145,15 @@ def get_airpods(message: Message) -> None:
     pods = search.get_products()
 
     for i in pods:
-        card = get_card(i)
+        card = Getting_Device.get_card(i)
         bot.send_message(message.chat.id, card)
 
 
-
 @bot.message_handler(func=lambda message: message.text == 'Apple Watch')
-def choose_aw_model(message: Message) -> None:
-    search = Apple_Watch()
-    kb = ReplyKeyboardMarkup(resize_keyboard=True)
-    kb.add(*search.models)
-    bot.send_message(
-        message.chat.id, 'Please, choose model', reply_markup=kb
-    )
-    bot.register_next_step_handler(message, input_mp_aw)
-
-
-def input_mp_aw(message: Message) -> None:
-    global model
-    model = message.text
-    bot.send_message(
-        message.chat.id, 'Enter the maximum allowable price (in rubles)'
-    )
-    bot.register_next_step_handler(message, input_diag)
-
-
-def input_diag(message: Message) -> None:
-    global max_price
-    try:
-        max_price = int(message.text)
-    except ValueError:
-        bot.reply_to(message, 'Incorrected input!')
-    else:
-        bot.send_message(
-            message.chat.id, 'Enter the screen diagonal (in millimeters)'
-        )
-        bot.register_next_step_handler(message, get_watches)
-
-def get_watches(message: Message) -> None:
-    global model, max_price, diagonal
-    try:
-        diagonal = int(message.text)
-    except ValueError:
-        bot.reply_to(message, 'Incorrected input!')
-    else:
-        bot.send_message(message.chat.id, 'Please, waiting...')
-        search = Apple_Watch()
-        watches = search.get_products(
-            url=str(search.models.get(model)),
-            max_price=int(max_price),
-            desired_diagonal=diagonal
-        )
-
-        for i in watches:
-            card = get_card(i)
-            bot.send_message(message.chat.id, card)
-    finally:
-        model, max_price, diagonal = '', 0, 0
+def get_aw(message):
+    aw = Apple_Watch()
+    search = Getting_Device(aw, apple_watch=True)
+    search.choose_model(message)
 
 
 
